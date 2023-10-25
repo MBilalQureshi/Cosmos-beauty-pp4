f2493615fc73f09fa7f2b7f65b0254ea307d1981
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-10-25 19:19:23 +0000

    Add inital database schema modals

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Address, UserPaymnet
+from .models import Address, UserPayment, ProductCategories, ProductDiscount, Product, CartItems, ConfirmedOrderDetails
 # Register your models here.
 admin.site.register(Address)
-admin.site.register(UserPaymnet)
+admin.site.register(UserPayment)
+admin.site.register(ProductCategories)
+admin.site.register(ProductDiscount)
+admin.site.register(Product)
+admin.site.register(CartItems)
+admin.site.register(ConfirmedOrderDetails)
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator, MaxValueValidator
+from cloudinary.models import CloudinaryField
+from autoslug import AutoSlugField
 
 
 # Address modal
@@ -19,7 +22,7 @@ class Address(models.Model):
         return f"{self.user.first_name} {self.user.last_name}"
 
 
-class UserPaymnet(models.Model):
+class UserPayment(models.Model):
     """User Payment Modal"""
     PAYMENT_METHOD_CHOICES = [
         (0, "COD(Cash on delivery)"),
@@ -39,3 +42,92 @@ class UserPaymnet(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+
+
+class ProductCategories(models.Model):
+    category_name = models.CharField(max_length=150, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        # ordering = ['created_on']
+
+    def __str__(self):
+        return self.category_name
+
+
+class ProductDiscount(models.Model):
+    discount_name = models.CharField(max_length=150, blank=False)
+    discount_percentage = models.PositiveIntegerField(validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)
+    ])
+    active = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.discount_name
+
+
+class Product(models.Model):
+    sku = models.CharField(max_length=16, blank=True)
+    name = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+    image = CloudinaryField('image', default='placeholder')
+    available = models.BooleanField(default=False)
+    stock = models.PositiveIntegerField(default=0, null=False)
+    product_category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE, related_name="product_category")
+    discount_name = models.ForeignKey(ProductDiscount, default=1, on_delete=models.SET_DEFAULT, related_name="product_discount")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
+
+
+class CartItems(models.Model):
+    user_info = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_info")
+    product_info = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="user_product_info")
+    quantity = models.PositiveIntegerField(blank=True, validators=[
+        MinValueValidator(1),
+        MaxValueValidator(10)
+    ])
+    extra_info = models.CharField(max_length=250, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.user_info.first_name} {self.user_info.last_name}"
+
+
+class ConfirmedOrderDetails(models.Model):
+    SHIPMENT_VIA = [
+        (0, 'DHL'),
+        (1, 'Hermes'),
+        (3, 'DPD')
+    ]
+    user_info = models.ForeignKey(User, on_delete=models.PROTECT, related_name="user_confirmed_order")
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    shipment_via = models.IntegerField(choices=SHIPMENT_VIA, default=0)
+    user_payment = models.OneToOneField(UserPayment, on_delete=models.PROTECT, related_name="user_payment_method")
+    order_status = models.CharField(max_length=50, default="Order Received")
+    # slug = AutoSlugField(populate_from='user_info', unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.user_info.first_name} {self.user_info.last_name}"
