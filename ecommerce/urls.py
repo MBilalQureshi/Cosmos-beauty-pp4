b8f53c7739e2afdf054a59b81306994710b5c0bf
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-11-01 15:52:09 +0000

    Add view for all product categories

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,5 +5,10 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('products/', views.Products.as_view(), name='products'),
-    path('makeup', views.Makeup.as_view(), name='makeup'),
+    path('makeup/', views.Makeup.as_view(), name='makeup'),
+    path('skincare/', views.SkinCare.as_view(), name='skincare'),
+    path('fragrence/', views.Fragrence.as_view(), name='fragrence'),
+    path('bathandbody/', views.Bathandbody.as_view(), name='bathandbody'),
+    path('hair/', views.Hair.as_view(), name='hair'),
+    path('specialoffers/', views.SpecialOffers.as_view(), name='specialoffers'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,12 +3,7 @@ from django.views import generic, View
 from .models import Address, UserPayment, ProductCategories, ProductDiscount, Product, CartItems, ConfirmedOrderDetails
 from django.utils import timezone
 
-# class a(View):
-#     def get_path(self,request):
-#         print('hello')
-#         current_url = resolve(request.path_info).url_name
-#         print(current_url)
-# Create your views here.
+
 class Home (generic.TemplateView):
     """
     """
@@ -16,30 +11,47 @@ class Home (generic.TemplateView):
 
 
 class Products (generic.ListView):
-#     def __init__(self, request):
-#         urlpath = request.path
     """
     Fetch products data from database and display on
     products.html
     """
-    # def get_context_data(self, **kwargs):
-    #     # Call the base implementation first to get a context
-    #     context = super().get_context_data(**kwargs)
-    #     # Add in a QuerySet of all the books
-    #     context["url_name"] = resolve(self.request.path_info).url_name
-    #     return context
-    # fetch url
-    # reduce the col required from db from next iteration
+    # TASK: CHECK IF DICOUNT EXIST OR NOT
     queryset = Product.objects.filter(available=True).filter(stock__gt=0).order_by('-created_on')
     template_name = 'products.html'
     paginate_by = 12
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["now"] = timezone.now()
-    #     return context
 
 
 class Makeup (generic.ListView):
     queryset = Product.objects.filter(available=True).filter(stock__gt=0).filter(product_category=2).order_by('-created_on')
+    template_name = 'products.html'
+    paginate_by = 12
+
+
+class SkinCare (generic.ListView):
+    queryset = Product.objects.filter(available=True).filter(stock__gt=0).filter(product_category=4).order_by('-created_on')
+    template_name = 'products.html'
+    paginate_by = 12
+
+
+class Hair (generic.ListView):
+    queryset = Product.objects.filter(available=True).filter(stock__gt=0).filter(product_category=3).order_by('-created_on')
+    template_name = 'products.html'
+    paginate_by = 12
+
+
+class Fragrence (generic.ListView):
+    queryset = Product.objects.filter(available=True).filter(stock__gt=0).filter(product_category=5).order_by('-created_on')
+    template_name = 'products.html'
+    paginate_by = 12
+
+
+class Bathandbody (generic.ListView):
+    queryset = Product.objects.filter(available=True).filter(stock__gt=0).filter(product_category=6).order_by('-created_on')
+    template_name = 'products.html'
+    paginate_by = 12
+
+
+class SpecialOffers (generic.ListView):
+    queryset = Product.objects.filter(available=True).filter(stock__gt=0).filter(discount_name__gt=1).order_by('-created_on')
     template_name = 'products.html'
     paginate_by = 12
