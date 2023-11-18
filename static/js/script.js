0e4ca193e806b29af5cf23525efe5c01b282e36f
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-11-18 18:14:10 +0000

    Fix qunatity increase/decrease message

--- static/js/script.js
+++ static/js/script.js
@@ -92,6 +92,11 @@ $(document).ready(function(){
         });
     });
 
+    $('#form-quntity').click(function(e) {
+        e.preventDefault();
+        $(".form-errors").hide();
+    });
+
     $('.btn-number').click(function(e){
         e.preventDefault();
         
@@ -132,36 +137,40 @@ $(document).ready(function(){
         maxValue =  parseInt($(this).attr('max'));
         valueCurrent = parseInt($(this).val());
         
-        name = $(this).attr('name');
+        let name = $(this).attr('name');
         if(valueCurrent >= minValue) {
+            console.log(1)
             $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
         } else {
-            alert('Sorry, the minimum value was reached');
+            // console.log(1)
+            // alert('Sorry, the minimum value was reached');
+            $(".form-errors").text('Sorry, the minimum value was reached').show();
             $(this).val($(this).data('oldValue'));
         }
         if(valueCurrent <= maxValue) {
+            console.log(2)
             $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
         } else {
-            alert('Sorry, the maximum value was reached');
+            // alert('Sorry, the maximum value was reached');
+            $(".form-errors").text('Sorry, the maximum value was reached').show();
             $(this).val($(this).data('oldValue'));
         }
         
-        
     });
-    $(".input-number").keydown(function (e) {
-            // Allow: backspace, delete, tab, escape, enter and .
-            if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-                 // Allow: Ctrl+A
-                (e.keyCode == 65 && e.ctrlKey === true) || 
-                 // Allow: home, end, left, right
-                (e.keyCode >= 35 && e.keyCode <= 39)) {
-                     // let it happen, don't do anything
-                     return;
-            }
-            // Ensure that it is a number and stop the keypress
-            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-                e.preventDefault();
-            }
-        });
+    // $(".input-number").keydown(function (e) {
+    //         // Allow: backspace, delete, tab, escape, enter and .
+    //         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+    //              // Allow: Ctrl+A
+    //             (e.keyCode == 65 && e.ctrlKey === true) || 
+    //              // Allow: home, end, left, right
+    //             (e.keyCode >= 35 && e.keyCode <= 39)) {
+    //                  // let it happen, don't do anything
+    //                  return;
+    //         }
+    //         // Ensure that it is a number and stop the keypress
+    //         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    //             e.preventDefault();
+    //         }
+    //     });
 
 });
