54fee4e397d15bb21e661d916abf28878aef18b1
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-11-22 18:41:55 +0000

    Update js file as per ES6 guideline

--- static/js/script.js
+++ static/js/script.js
@@ -1,38 +1,14 @@
 $(document).ready(function(){
-
-    // remove messages in 3 seconds
-    // setTimeout(function(){
-    //     let messages = document.getElementById("msg");
-
-    //     if (messages) {
-    //         let alert = new bootstrap.Alert(messages);
-    //         alert.close();
-    //     }
-    // },3000);
-
-    // Add/remove from wishlist
-    $(document).on("scroll", function() {
-        var pageTop = $(document).scrollTop();
-        var pageBottom = pageTop + $(window).height();
-        var tags = $(".tag");
-    
-        tags.each(function() {
-            var tag = $(this);
-            if (tag.position().top < pageBottom) {
-                tag.addClass("visible");
-            } else {
-                tag.removeClass("visible");
-            }
-        });
-    });
-      
-
+    /**
+     * Add / remove products to wishlist AJAX handler.
+     * Concept of AJAX handling is learned from
+     * 
+     */
     $('.add-to-wishlist').click(function(e){
         e.preventDefault();
-
         $(this).text(function(i, text){
-            text = text.trim()
-            return text == 'Add to wishlist' ? 'Remove from wishlist' : 'Add to wishlist'
+            text = text.trim();
+            return text == 'Add to wishlist' ? 'Remove from wishlist' : 'Add to wishlist';
         });
         let productId = $(this).attr("value");
         let token = $('input[name=csrfmiddlewaretoken]').val();
@@ -43,30 +19,26 @@ $(document).ready(function(){
                 productId: productId,
                 csrfmiddlewaretoken: token
             },
-            // // success: function(json){},
-            // // error: function(xhr, errmsg, err){}
             success: function (response){
-                // console.log(response)
-                // console.log(response.status)
             }
         });
     });
 
-    // Add/remove from cart
+
+    /**
+     * Add / remove products to cart AJAX handler.
+     */
     $('.add-to-cart').click(function(e){
         e.preventDefault();
         $(this).text(function(i, text){
-            text = text.trim()
-            return text == 'Add to cart' ? 'Remove from cart' : 'Add to cart'
+            text = text.trim();
+            return text == 'Add to cart' ? 'Remove from cart' : 'Add to cart';
         });
         let productId = $(this).attr("value");
         let token = $('input[name=csrfmiddlewaretoken]').val();
         let productQuantity = parseInt($('.input-number').val());
-        console.log(productQuantity)
         let price = $('.product-price').html();
-        console.log(price)
         let discount = $('.product-discount').html();
-        console.log(discount)
         $.ajax({
             method: "POST",
             url:'/add-to-cart/',
@@ -77,18 +49,17 @@ $(document).ready(function(){
                 price:price,
                 discount:discount,
             },
-            // // success: function(json){},
-            // // error: function(xhr, errmsg, err){}
             success: function (response){
-                // console.log(response)
-                // console.log(response.status)
             }
         });
     });
 
-    $('.product-remove').click(function(e){    
+
+    /**
+     * Product removal handler from with in cart.
+     */
+    $('.product-remove').click(function(e){
         e.preventDefault();
-        console.log(12232)
         let productId = $(this).attr("value");
         $(`.${productId}`).remove();
         $('#continue-btn').css('display', 'none');
@@ -100,28 +71,55 @@ $(document).ready(function(){
                 productId: productId,
                 csrfmiddlewaretoken: token,
             },
-            // // success: function(json){},
-            // // error: function(xhr, errmsg, err){}
             success: function (response){
-                // $('#continue-btn').prop('disabled', false);
-                
-                window.location.reload()
-                // console.log(response)
-                // console.log(response.status)
+                window.location.reload();
             }
         });
     });
 
+
+    /**
+     * This ready to use animation code is taken from ''
+     * The effect can be seen above footer on home page.
+     */
+    $(document).on("scroll", function() {
+        let pageTop = $(document).scrollTop();
+        let pageBottom = pageTop + $(window).height();
+        let tags = $(".tag"); 
+        tags.each(function() {
+            let tag = $(this);
+            if (tag.position().top < pageBottom) {
+                tag.addClass("visible");
+            } else {
+                tag.removeClass("visible");
+            }
+        });
+    });
+
+
+    /**
+     * This event removes warnings once user click on
+     * quantity input field on product details page.
+     */
     $('#form-quntity').click(function(e) {
         e.preventDefault();
         $(".form-errors").hide();
     });
 
+
+    /**
+     * Below code till very end handles the quantity on products detail page.
+     * This code is required as this input was not part of form
+     * and input was handled by session later. So input handling
+     * was required. This ready to use code is taken from :
+     * depricated code was fixed and remaining code was updated
+     * as needed.
+     */
     $('.btn-number').click(function(e){
         e.preventDefault();
         
-        fieldName = $(this).attr('data-field');
-        type      = $(this).attr('data-type');
+        let fieldName = $(this).attr('data-field');
+        let type = $(this).attr('data-type');
         var input = $("input[name='"+fieldName+"']");
         var currentVal = parseInt(input.val());
         if (!isNaN(currentVal)) {
@@ -133,7 +131,6 @@ $(document).ready(function(){
                 if(parseInt(input.val()) == input.attr('min')) {
                     $(this).attr('disabled', true);
                 }
-    
             } else if(type == 'plus') {
     
                 if(currentVal < input.attr('max')) {
@@ -142,55 +139,34 @@ $(document).ready(function(){
                 if(parseInt(input.val()) == input.attr('max')) {
                     $(this).attr('disabled', true);
                 }
-    
             }
         } else {
             input.val(0);
         }
     });
+
     $('.input-number').focusin(function(){
        $(this).data('oldValue', $(this).val());
     });
 
-    $('.input-number').change(function() {
-        
-        minValue =  parseInt($(this).attr('min'));
-        maxValue =  parseInt($(this).attr('max'));
-        valueCurrent = parseInt($(this).val());
-        console.log(valueCurrent + 'asdasd')
+    $('.input-number').change(function() {    
+        let minValue =  parseInt($(this).attr('min'));
+        let maxValue =  parseInt($(this).attr('max'));
+        let valueCurrent = parseInt($(this).val());
         let name = $(this).attr('name');
 
+
         if(valueCurrent >= minValue) {
-            $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+            $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled');
         } else {
-            // console.log(1)
-            // alert('Sorry, the minimum value was reached');
-            $(".form-errors").text('Sorry, the minimum value was reached').show();
+            $(".form-errors").text('Sorry, kindly enter between 1 - 10').show();
             $(this).val($(this).data('oldValue'));
         }
         if(valueCurrent <= maxValue) {
-            $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+            $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled');
         } else {
-            // alert('Sorry, the maximum value was reached');
-            $(".form-errors").text('Sorry, the maximum value was reached').show();
+            $(".form-errors").text('Sorry,  kindly enter between 1 - 10').show();
             $(this).val($(this).data('oldValue'));
         }
-        
     });
-    // $(".input-number").keydown(function (e) {
-    //         // Allow: backspace, delete, tab, escape, enter and .
-    //         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-    //              // Allow: Ctrl+A
-    //             (e.keyCode == 65 && e.ctrlKey === true) || 
-    //              // Allow: home, end, left, right
-    //             (e.keyCode >= 35 && e.keyCode <= 39)) {
-    //                  // let it happen, don't do anything
-    //                  return;
-    //         }
-    //         // Ensure that it is a number and stop the keypress
-    //         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-    //             e.preventDefault();
-    //         }
-    //     });
-
 });
